0a8b76329ddf00d13ff44a3b9c12367124f81dd4
bastard <chrusty@gmail.com>
2015-10-30 17:21:09 +0000

    Basic subscription and tail works

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	log "github.com/cihub/seelog"
+
+	context "golang.org/x/net/context"
+	google "golang.org/x/oauth2/google"
+	pubsub "google.golang.org/api/pubsub/v1"
+)
+
+var (
+	project   = flag.String("project", "", "Google project-name")
+	topic     = flag.String("topic", "", "PUB/SUB topic name to subscribe to")
+	batchsize = flag.Int64("batchsize", 1, "How many messages to get at once")
+)
+
+func init() {
+	// Parse the command-line arguments:
+	flag.Parse()
+}
+
+func main() {
+	// Make sure we flush the log before quitting:
+	defer log.Flush()
+
+	// Connect to GCE (either from GCE permissions, JSON file, or ENV-vars):
+	client, err := google.DefaultClient(context.Background(), pubsub.PubsubScope)
+	if err != nil {
+		log.Criticalf("Unable to authenticate to GCE! (%s)", err)
+		os.Exit(2)
+	}
+
+	// Get a DNS service-object:
+	pubsubService, err := pubsub.New(client)
+	if err != nil {
+		log.Criticalf("Failed to connect to PUB/SUB! %v", err)
+		os.Exit(2)
+	}
+
+	// Build the topic and subscription names:
+	fullTopicName := fmt.Sprintf("projects/%v/topics/%v", *project, *topic)
+	fullSubscriptionName := fmt.Sprintf("projects/%v/subscriptions/%v", *project, "tail-pubsub")
+
+	// Create a subscription:
+	sub := &pubsub.Subscription{Topic: fullTopicName}
+	subscription, err := pubsubService.Projects.Subscriptions.Create(fullSubscriptionName, sub).Do()
+	if err != nil {
+		log.Warnf("Failed to create subscription to topic (%v): %v", fullTopicName, err)
+		subscription, err = pubsubService.Projects.Subscriptions.Get(fullSubscriptionName).Do()
+		if err != nil {
+			log.Criticalf("Couldn't even get existing subscription (%v): %v", fullSubscriptionName, err)
+			os.Exit(2)
+		}
+	}
+	log.Debugf("Subscription (%s) was created", subscription.Name)
+
+	// Prepare a pull-request:
+	pullRequest := &pubsub.PullRequest{
+		ReturnImmediately: false,
+		MaxMessages:       *batchsize,
+	}
+
+	// Now attack the pub/sub API:
+	for {
+		pullResponse, err := pubsubService.Projects.Subscriptions.Pull(fullSubscriptionName, pullRequest).Do()
+		if err != nil {
+			log.Errorf("Failed to pull messages from subscription (%v): %v", fullSubscriptionName, err)
+		} else {
+			for _, receivedMessage := range pullResponse.ReceivedMessages {
+				messageData, err := base64.StdEncoding.DecodeString(receivedMessage.Message.Data)
+				if err != nil {
+					log.Warnf("Failed to decode message: %v", err)
+				} else {
+					log.Infof("Message: %v", messageData)
+					ackRequest := &pubsub.AcknowledgeRequest{
+						AckIds: []string{receivedMessage.AckId},
+					}
+					if _, err = pubsubService.Projects.Subscriptions.Acknowledge(fullSubscriptionName, ackRequest).Do(); err != nil {
+						log.Warnf("Failed to acknowledge messages: %v", err)
+					}
+				}
+			}
+		}
+
+		time.Sleep(1 * time.Second)
+
+	}
+
+}
